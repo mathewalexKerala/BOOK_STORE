891aeb3a0afb9a980b54d86c74fa479572629c36
Mathew_Alex <116719115+mathewalexKerala@users.noreply.github.com>
2024-08-13 23:05:37 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('http://localhost:5555/books')
+      .get('https://book-store-x51t.vercel.app/books')
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
